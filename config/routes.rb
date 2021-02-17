ca6b8be2f59dbb3858f22b19f434b290df674852
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-02-17 16:31:08 +0100

    complete without review embed in rest. show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+
+
+
+# see all restaurants :         GET "restaurants"       index  .all
+
+# see view of new restaurant    GET "restaurants/new"   new  .new()
+# create new rewstaurant        POST "restaurants"      create .new&.save or .create
+
+# details of a restaurant,      GET "restaurants/38"     show .find()
+# with all the reviews
+
+# A visitor can add a           GET "restaurants/38/reviews/new" ??
+# new review to a restaurant    POST "restaurants/38/reviews"   ??
+
+
+
