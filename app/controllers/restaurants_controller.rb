7df8ad9e41ccc2d8d06e9b967fd8023427129df9
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-02-17 18:15:53 +0100

    added rating system inside rest show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show]
+  # before_action :set_restaurant, only: [:show]
   def index
     @restaurants = Restaurant.all
   end
@@ -19,14 +19,15 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-
+      @restaurant = Restaurant.find(params[:id])
+      @review = Review.new
   end
 
   private
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
-  end
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
